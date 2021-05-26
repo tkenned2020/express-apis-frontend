66830d71e6412a429d8e20750312bec754031deb
tkenned2020 <tkenned2020@outlook.com>
2021-05-26 17:45:16 -0400

    phase 4 part one in progress

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,9 @@
-console.log("Hello from index.js!");
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+      const res = await fetch("http://localhost:8080/tweets");
+      const { tweets } = await res.json();
+      console.log(tweets);
+    } catch (e) {
+      console.error(e);
+    }
+  });
