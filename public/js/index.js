fc0bc52a3a4047da176f37d3837dee0a9bba3725
Eduardo <eduardosh1105@gmail.com>
2021-05-27 14:57:21 -0600

    Finished Phase 3

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,30 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  try {
-    const res = await fetch("http://localhost:8080/tweets");
-    const { tweets } = await res.json();
+	try {
+		const res = await fetch("http://localhost:8080/tweets", {
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem(
+					"TWITTER_LITE_ACCESS_TOKEN"
+				)}`,
+			},
+		});
+		const { tweets } = await res.json();
 
-    const tweetsContainer = document.querySelector("#tweets-container");
-    const tweetsHtml = tweets.map(
-      ({ message }) => `
+		const tweetsContainer = document.querySelector("#tweets-container");
+		const tweetsHtml = tweets.map(
+			({ message }) => `
     <div class="card">
       <div class="card-body">
         <p class="card-text">${message}</p>
       </div>
     </div>
   `
-    );
-    tweetsContainer.innerHTML = tweetsHtml.join("");
-  } catch (e) {
-    console.error(e);
-    if (res.status === 401) {
-      window.location.href = "/log-in";
-      return;
-    }
-  }
+		);
+		tweetsContainer.innerHTML = tweetsHtml.join("");
+	} catch (e) {
+		// console.error(e);
+		if (res.status === 401) {
+			window.location.href = "/log-in";
+			return;
+		}
+	}
 });
-
-// app.get("/sign-up", (req, res) => {
-//   res.render("sign-up");
-// });
